f51dc4911ece5defda3958f428c2f8f5346b788f
Ekhsondzhon Boboev <ehsonboboev@gmail.com>
2020-09-26 04:59:30 +0500

    v2

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -40,22 +40,4 @@ func TestCategoriesAvg(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 
-
-func TestFilterByCategory_foundOne(t *testing.T) {
-	payments := []types.Payment{
-		{ID:1, Category: "auto", Amount: 1_000_00},
-		{ID:2, Category: "food"},
-		
-		
-		
-	}
-	expected := []types.Payment{
-	
-		{ID: 2, Category: "food"},
-		
-	}
-	result := FilterByCategory(payments, "food")	
-	if !reflect.DeepEqual(expected, result) {
-		t.Error("invalid result")
-	}
-
+}
