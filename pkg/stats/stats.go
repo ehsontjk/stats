29fac390c2cb2872f9aabe44c08a31e13d8c9cd0
Ekhsondzhon Boboev <ehsonboboev@gmail.com>
2020-09-25 21:00:13 +0500

    v2

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -37,3 +37,10 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 	}
 
 
+	func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+		categories := map[types.Category]types.Money{}
+		for _, payment := range payments {
+			categories[payment.Category] += payment.Amount
+		}
+		return categories
+	}
